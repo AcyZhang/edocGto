76ec55d9847affd99272f8f7807a02241150c38d
AcyZhang <18735720677@163.com>
2018-06-25 17:55:34 +0800

    uplifile

--- src/java/com/edocGorty/Utils/osaUtils.java
+++ src/java/com/edocGorty/Utils/osaUtils.java
@@ -173,7 +173,6 @@ public class osaUtils {
      */
     public String getUrl(String key) {
         // 设置URL过期时间为10年 3600l* 1000*24*365*10
-
         Date expiration = new Date(System.currentTimeMillis() + 3600L * 1000 * 24 * 365 * 10);
         // 生成URL
         URL url = ossClient.generatePresignedUrl(bucketName, key, expiration);
